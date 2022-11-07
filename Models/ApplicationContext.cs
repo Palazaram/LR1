92c0d2699b1721298531d12406dabe2e6d971990
Антон Ворошилов <108758569+Palazaram@users.noreply.github.com>
2022-11-07 22:54:57 +0200

    LR1
    LR1

---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NewWebShopApp.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; } = null!;
+
+        public DbSet<Category> Categories { get; set; } = null!;
+
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
+    }
+}
